4969f6e274c445b8f73371b8160321e4c4fc2eb9
Fred Meziere <Fredzem@outlook.fr>
2023-06-07 19:07:26 +0200

    feat: Ajout d'un premier composant servant pour l'introduction du site

---
+++ src/components/Main/introduction/index.js
@@ -0,0 +1,16 @@
+import './styles.scss';
+
+function introduction() {
+  return (
+    <div className="div__intro__main">
+      <h2 className="h2__div__main">Développeur web
+        <br />
+        <span className="span__2__main__div__h2">Fullstack JS</span>
+        <br />
+        <span className="span__main__div__h2">Orienté back-end</span> <span className="span__point1__main">.</span>
+        <span className="span__point2__main">.</span><span className="span__point3__main">.</span>
+      </h2>
+    </div>
+  );
+}
+export default introduction;
