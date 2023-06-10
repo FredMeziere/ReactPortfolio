214643ce3b14323893c697c9d4fe90a9485bbf91
Fred Meziere <Fredzem@outlook.fr>
2023-06-10 20:02:03 +0200

    Feat: Ajouts de divers responsive et ajout d'un bouton pour gagner en place sur les versions mobiles

--- src/components/Main/realisations/Cards/index.js
+++ src/components/Main/realisations/Cards/index.js
@@ -1,23 +1,37 @@
-import {
-  arrayOf, shape,
-} from 'prop-types';
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { arrayOf, shape } from 'prop-types';
 import Card from './Card';
 import './styles.scss';
 
 function Cards({ datas }) {
+  const isMobile = useMediaQuery({ maxWidth: 420 });
+  const [visibleCardIndex, setVisibleCardIndex] = useState(0);
+
+  const handleNextCard = () => {
+    setVisibleCardIndex((prevIndex) => (prevIndex + 1) % datas.length);
+  };
+
   return (
     <div className="cards">
-      {
-        datas.map((data) => (
-          <Card key={data.id} {...data} />
-        ))
-      }
+      {isMobile ? (
+        <div>
+          {datas.slice(visibleCardIndex, visibleCardIndex + 1).map((data) => (
+            <Card key={data.id} {...data} />
+          ))}
+        </div>
+      ) : (
+        datas.map((data) => <Card key={data.id} {...data} />)
+      )}
+      <button className="cards-button" type="button" onClick={handleNextCard}>
+        Suivant
+      </button>
     </div>
   );
 }
 
-export default Cards;
-
 Cards.propTypes = {
   datas: arrayOf(shape()).isRequired,
 };
+
+export default Cards;
