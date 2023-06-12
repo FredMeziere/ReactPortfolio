338f2a1436c1bb171b9eaa2dfd62ddc72edd3256
Fred Meziere <Fredzem@outlook.fr>
2023-06-12 10:34:18 +0200

    feat : Ajout de responsive supplémentaire ainsi que menu burger fonctionnel

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,47 @@
-import { useState } from 'react';
 import './styles.scss';
+import { useEffect, useState } from 'react';
+import MobileNavbar from '../MobileNavbar';
 
 function Header() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 912px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = () => {
+      setIsMobile(mediaQuery.matches);
+    };
+
+    mediaQuery.addListener(handleResize);
+
+    return () => {
+      mediaQuery.removeListener(handleResize);
+    };
+  }, []);
 
   return (
     <div className="header">
       <div className="header-div">
         <a className="header-h1color" href="/main">
           <h1 className="header-h1">
-            <span className="header-h1span">F</span>red<span className="header-h1point">.</span><span className="header-h1spanm">M</span>eziere
+            <span className="header-h1span">F</span>red<span className="header-h1point">.</span>
+            <span className="header-h1spanm">M</span>eziere
           </h1>
         </a>
       </div>
 
-      <div className={`navbar desktop-navbar ${isMenuOpen ? 'open' : ''}`}>
+      {!isMobile && (
+      <div className="navbar">
         <p className="navbar-plink1"><a className="navbar-a1" href="#">Accueil</a></p>
         <p className="navbar-plink2-3"><a className="navbar-a2" href="#skills">Mes Compétences</a></p>
         <p className="navbar-plink2-3"><a className="navbar-a3" href="#realisation">Mes réalisations</a></p>
         <p className="navbar-plink2-3"><a className="navbar-a3" href="#presentation">Mon profil</a></p>
-        <p className="navbar-plink4"><a className="navbar-a4" href="#recrutezmoi">Contactez-Moi</a></p>
+        <p className="navbar-plink4"><a className="navbar-a4" href="#recrutezmoi">Contactez-moi</a></p>
       </div>
+      )}
 
-      <div className={`navbar mobile-navbar ${isMenuOpen ? 'open' : ''}`}>
-        <div className="menu-burger" onClick={toggleMenu}>
-          <span className="burger-line" />
-          <span className="burger-line" />
-          <span className="burger-line" />
-        </div>
-      </div>
+      {isMobile && <MobileNavbar />}
     </div>
   );
 }
---
+++ src/components/MobileNavbar/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './styles.scss';
+
+function MobileNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="mobilenavbar">
+      <button className="mobilenavbar-toggle" onClick={toggleMenu} type="button">
+        <span className="mobilenavbar-toggle-line" />
+        <span className="mobilenavbar-toggle-line" />
+        <span className="mobilenavbar-toggle-line" />
+      </button>
+
+      {isMenuOpen && (
+        <ul className="mobilenavbar-ul">
+          <li><a className="mobilenavbar-li" href="#">Accueil</a></li>
+          <li><a className="mobilenavbar-li" href="#skills">Mes Compétences</a></li>
+          <li><a className="mobilenavbar-li" href="#realisation">Mes réalisations</a></li>
+          <li><a className="mobilenavbar-li" href="#presentation">Mon profil</a></li>
+          <li><a className="mobilenavbar-li" href="#recrutezmoi">Contactez-Moi</a></li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default MobileNavbar;
